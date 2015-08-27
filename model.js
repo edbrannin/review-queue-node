a6080d1b5e1da0602f48b370391bd17b015c3877
Ed Brannin <edbrannin@gmail.com>
2015-08-26 22:41:14 -0400

    Added model plans

--- model.js
+++ model.js
@@ -10,5 +10,51 @@ var Person = sequelize.define('Person', {
   tableName: "people"
 });
 
+/*
+ * TODO: Add the following models
+ *
+ * Source?
+ * - name
+ * - source_code
+ * - url
+ *
+ * Item
+ * * source_code
+ * * source_primary_id
+ *
+ * ItemVersion
+ * - item_id
+ * - name
+ * - version
+ * - size_compressed_bytes
+ * - size_uncompressed_bytes
+ * - description
+ *
+ * ItemIdentifier
+ * - item_id
+ * - identifier_name
+ * - value
+ *
+ * ItemLink
+ * - item_id
+ * - link_type_name
+ * - url
+ *
+ * Tag
+ * - name
+ *
+ * ItemTag
+ * - item_id
+ * - tag_id
+ * - created_by => Person.id
+ *
+ * ItemNote
+ * - item_id
+ * - created_by
+ * - note
+ *
+ *
+ */
+
 exports.sequelize = sequelize;
 exports.Person = Person;
