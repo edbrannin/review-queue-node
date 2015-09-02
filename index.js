28b703bf773fdd60d7a9ad323d98c872c0ab9dbc
Ed Brannin <edbrannin@gmail.com>
2015-09-01 23:06:09 -0400

    Got the source-scanning bit fixed again

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var model = require("./model.js");
 
 var itunes_source = require('./sources/itunes.js');
 
-itunes_source.scan(model, function() {
+itunes_source.scan(model, function(model, iTunes) {
+  //console.log("Callback got:", iTunes);
   process.exit();
 });
 
--- sources/itunes.js
+++ sources/itunes.js
@@ -1,4 +1,30 @@
+var fs = require('fs'),
+    path = require('path');
+
 var SOURCE_CODE = 'ITUNES';
+
+/*
+ * http://stackoverflow.com/questions/9080085/node-js-find-home-directory-in-platform-agnostic-way
+ */
+function getUserHome() {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
+
+function find_itunes_root() {
+  paths = [
+    "Music/iTunes/iTunes Music/"
+  ]
+  for (var x = 0; x < paths.length; x+= 1) {
+    var path = path.join(getUserHome(), paths[x])
+    if (fs.existsSync(path)) {
+      return path;
+    }
+  }
+}
+
+function mobile_apps_dir() {
+  return path.join(find_itunes_root(), "Mobile Applications");
+}
 
 function source(model, callback) {
   model.Source.query().where({
@@ -6,23 +32,22 @@ function source(model, callback) {
   }).then(function(row) {
     if (row.length == 0) {
       var now = Date.now();
-      var iTunes = model.Source.query().insert({
+      model.Source.query().insert({
         source_code: SOURCE_CODE,
         name: "iTunes App Store",
         created_at: now,
-        updatd_at: now,
+        updated_at: now,
         // TODO URL?
-      });
+      }).then(callback);
     } else {
-      var iTunes = row[0];
+      callback(row[0]);
     }
-    callback(iTunes);
   });
 }
 
 exports.scan = function(model, callback, progress_callback) {
   source(model, function(iTunes) {
-    console.log("Got Source ", iTunes);
-    callback(model);
+    //console.log("Got Source ", iTunes);
+    callback(model, iTunes);
   });
 }
