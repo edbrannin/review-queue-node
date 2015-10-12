08dcc19763a620b54b9b4793d85c86687c20136c
Ed Brannin <edbrannin@gmail.com>
2015-10-11 20:27:08 -0400

    Started writing queue.json's route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var model = require('../model.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bridget' });
+});
+
+router.get('/queue.json', function(req, res, next) {
+  var q = model.ItemVersion.query()
+    .limit(12)
+    .orderBy('size_compressed_bytes', 'desc')
+    .groupBy('item_id')
+    //.having('')
+    .eager('item');
+  //TODO Filter by tags
+  q.then(function(items) {
+    items = items.map(function(item) {
+      item.tags = [];
+    });
+    res.json(items);
+  });
 });
 
 module.exports = router;
